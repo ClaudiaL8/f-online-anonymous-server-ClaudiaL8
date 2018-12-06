68e1e8113cce8cdbc91ba643098da4fe3403cd9f
Claudia Calero <cl.calerolopez@gmail.com>
2018-12-06 14:35:03 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,20 +16,3 @@ function closeMenu(){
 
 boton.addEventListener('click', openMenu);
 botonCerrar.addEventListener('click', closeMenu);
-
-
-// var menuIcon = document.querySelector('.nav-trigger');
-// var cross = document.querySelector('.nav-close');
-// var items = document.querySelector('.nav');
-
-// function showMenu (){
-//   items.classList.add('nav__open');
-//   menuIcon.classList.add('trigger__out');
-//   items.classList.add('nav__animation');
-// }
-// function hideMenu() {
-//   items.classList.remove('nav__open');
-//   menuIcon.classList.remove('trigger__out');
-// }
-// menuIcon.addEventListener('click', showMenu);
-// cross.addEventListener('click', hideMenu);
